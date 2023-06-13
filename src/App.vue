66c8e16eb2df3c9e470444ae75ae585a45042b19
Szevasz <Szevasz@163.com>
2023-06-13 16:53:12 +0800

    chore: src alias

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <el-button type="primary" size="default" @click="" :icon="Plus">主要按钮</el-button>
-    <el-button type="success" size="default" @click="" :icon="Edit">编辑</el-button>
-    <el-button type="danger" size="default" @click="" :icon="Delete">删除</el-button>
-    
-    
+    <el-button type="primary" size="default" @click="" :icon="Plus">
+      主要按钮
+    </el-button>
+    <el-button type="success" size="default" @click="" :icon="Edit">
+      编辑
+    </el-button>
+    <el-button type="danger" size="default" @click="" :icon="Delete">
+      删除
+    </el-button>
   </div>
+  <test />
 </template>
 
 <script setup lang="ts">
- import {Plus,Edit,Delete} from '@element-plus/icons-vue'
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
+import test from '@/components/test.vue'
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 //引入element-plus插件与样式
-import ElementPlus from 'element-plus';
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //配置element-plus国际化
 //@ts-ignore
@@ -9,8 +9,8 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 //获取应用实例对象
 const app = createApp(App)
 //安装element-plus插件
-app.use(ElementPlus,{
+app.use(ElementPlus, {
   locale: zhCn,
-});
+})
 //将应用挂载到挂载点
 app.mount('#app')
