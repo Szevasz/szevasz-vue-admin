484eeeaa134c349000bb12ef5cdfbd28f8141fd4
Szevasz <Szevasz@163.com>
2023-06-27 13:19:04 +0800

    feat: Menu static component construction

--- src/setting.ts
+++ src/setting.ts
@@ -1,6 +1,6 @@
 //用于logo|标题配置
-export default{
-    title:'Szevasz Admin',
-    logo:'/public/logo.png',
-    logoHidden:true
+export default {
+  title: 'Szevasz Admin',
+  logo: '/public/logo.png',
+  logoHidden: true,
 }
