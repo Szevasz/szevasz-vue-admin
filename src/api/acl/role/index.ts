59763687bbe8367d0baef8af5734dfc62bb9ba53
Szevasz <Szevasz@163.com>
2023-09-30 21:28:29 +0800

    feat: Data presentation of the role module

---
+++ src/api/acl/role/index.ts
@@ -0,0 +1,23 @@
+//角色管理模块的的接口
+import request from '@/utils/request'
+import type { RoleResponseData, RoleData } from './type'
+//枚举地址
+enum API {
+  //获取全部的职位接口
+  ALLROLE_URL = '/admin/acl/role/',
+  //新增岗位的接口地址
+  ADDROLE_URL = '/admin/acl/role/save',
+  //更新已有的职位
+  UPDATEROLE_URL = '/admin/acl/role/update',
+  //获取全部的菜单与按钮的数据
+  ALLPERMISSTION = '/admin/acl/permission/toAssign/',
+  //给相应的职位分配权限
+  SETPERMISTION_URL = '/admin/acl/permission/doAssign/?',
+  //删除已有的职位
+  REMOVEROLE_URL = '/admin/acl/role/remove/',
+}
+//获取全部的角色
+export const reqAllRoleList = (page: number, limit: number, roleName: string) =>
+  request.get<any, RoleResponseData>(
+    API.ALLROLE_URL + `${page}/${limit}/?roleName=${roleName}`,
+  )
--- src/views/acl/role/index.vue
+++ src/views/acl/role/index.vue
@@ -2,30 +2,40 @@
   <el-card>
     <el-form :inline="true" class="form">
       <el-form-item label="职位搜索">
-        <el-input placeholder="请你输入职位关键词"></el-input>
+        <el-input placeholder="请你输入职位关键词" v-model="keyword"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" size="default" @click="search">搜索</el-button>
+        <el-button type="primary" size="default" @click="search" :disabled="keyword?false:true">
+          搜索
+        </el-button>
         <el-button type="primary" size="default" @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </el-card>
-  <el-card style="margin: 10px 0px;">
-    <el-button type="primary" size="default" icon="Plus" @click="">添加职位</el-button>
-    <el-table border style="margin: 10px 0px;">
+  <el-card style="margin: 10px 0px">
+    <el-button type="primary" size="default" icon="Plus" @click="">
+      添加职位
+    </el-button>
+    <el-table border style="margin: 10px 0px" :data="allRole">
       <el-table-column type="index" align="center" label="#"></el-table-column>
       <el-table-column label="ID" align="center" prop="id"></el-table-column>
       <el-table-column label="职位名称" align="center" prop="roleName" show-overflow-tooltip></el-table-column>
-      <el-table-column label="创建世间" align="center" show-overflow-tooltip prop="createTime"></el-table-column>
+      <el-table-column label="创建时间" align="center" show-overflow-tooltip prop="createTime"></el-table-column>
       <el-table-column label="更新时间" align="center" show-overflow-tooltip prop="updateTime"></el-table-column>
       <el-table-column label="操作" width="280px" align="center">
         <!-- row:已有的职位对象 -->
         <template #="{ row, $index }">
-          <el-button type="primary" size="small" icon="User" @click="setPermisstion(row)">分配权限</el-button>
-          <el-button type="primary" size="small" icon="Edit" @click="updateRole(row)">编辑</el-button>
+          <el-button type="primary" size="small" icon="User" @click="setPermisstion(row)">
+            分配权限
+          </el-button>
+          <el-button type="primary" size="small" icon="Edit" @click="updateRole(row)">
+            编辑
+          </el-button>
           <el-popconfirm :title="`你确定要删除${row.roleName}?`" width="260px" @confirm="removeRole(row.id)">
             <template #reference>
-              <el-button type="primary" size="small" icon="Delete">删除</el-button>
+              <el-button type="primary" size="small" icon="Delete">
+                删除
+              </el-button>
             </template>
           </el-popconfirm>
         </template>
@@ -38,18 +48,56 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted} from 'vue'
+//请求方法
+import { reqAllRoleList } from '@/api/acl/role';
+import { Records, RoleResponseData } from '@/api/acl/role/type';
 //当前页码
-let pageNo = ref<number>(1);
+let pageNo = ref<number>(1)
 //一页展示几条数据
-let pageSize = ref<number>(10);
+let pageSize = ref<number>(10)
+//搜索职位关键字
+let keyword = ref<string>('');
+//存储全部已有的职位
+let allRole = ref<Records>([]);
+//职位总个数
+let total = ref<number>(0);
+//组件挂载完毕
+onMounted(() => {
+    //获取职位请求
+    getHasRole();
+});
+//获取全部用户信息的方法|分页器当前页码发生变化的回调
+const getHasRole = async (pager = 1) => {
+    //修改当前页码
+    pageNo.value = pager;
+    let result: RoleResponseData = await reqAllRoleList(pageNo.value, pageSize.value, keyword.value);
+    if (result.code == 200) {
+        total.value = result.data.total;
+        allRole.value = result.data.records;
+    }
+}
+//下拉菜单的回调
+const sizeChange = () => {
+    getHasRole();
+}
+//搜索按钮的回调
+const search = () => {
+  getHasRole()
+  keyword.value = ''
+}
+//重置按钮的回调
+const reset = () => {
+  keyword.value = ''
+  getHasRole()
+}
 </script>
 
 <style scoped>
 .form {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
 }
 </style>
